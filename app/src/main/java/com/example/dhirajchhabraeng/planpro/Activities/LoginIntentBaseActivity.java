ad20f4beabdfc7d948c9f46007d86a26e854c2bb
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-10-02 22:08:33 +0530

    commit for base activity, login intent using FirebaseAuth, handling of no user login(showing Toast for that) and a sample Main Screen having a logout button for testing

--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/LoginIntentBaseActivity.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/LoginIntentBaseActivity.java
@@ -1,15 +1,96 @@
 package com.example.dhirajchhabraeng.planpro.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.dhirajchhabraeng.planpro.R;
+import com.example.dhirajchhabraeng.planpro.StorageManagement.PrefManager;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginIntentBaseActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class LoginIntentBaseActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+
+    private PrefManager prefManager;
+    private FirebaseAuth firebaseAuth;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            launchHomeScreen();
+            finish();
+        }
+
         setContentView(R.layout.activity_login_intent_base);
+
+        btnLogin = findViewById(R.id.login_button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.addAuthStateListener(LoginIntentBaseActivity.this);
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onResume() {
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            launchHomeScreen();
+            finish();
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && firebaseAuth.getCurrentUser() == null) {
+            Toast.makeText(this, "The application needs to be logged in to continue", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        if (firebaseAuth.getCurrentUser() == null) {
+            firebaseAuth.removeAuthStateListener(this);
+
+            Intent loginIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setIsSmartLockEnabled(false)
+                    .setAvailableProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.PhoneBuilder().build()))
+                    .build();
+
+            startActivityForResult(loginIntent, 0);
+        } else {
+
+        }
+    }
+
+    private void launchHomeScreen() {
+        Intent intent = new Intent(LoginIntentBaseActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/MainActivity.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.dhirajchhabraeng.planpro.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dhirajchhabraeng.planpro.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        logoutButton = findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.putExtra("logout", 0);
+                startActivity(i);
+
+                finish();
+            }
+        });
     }
 }
