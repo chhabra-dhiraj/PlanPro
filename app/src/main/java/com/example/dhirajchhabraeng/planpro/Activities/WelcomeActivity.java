793dfdf5b4838e719b0c87525f247a9abaab965d
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-10-03 00:40:19 +0530

    commit after mimmicking the manifest of damanpreet and adding SplashScreen and WelcomeActivity to Activities Package

---
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.dhirajchhabraeng.planpro.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dhirajchhabraeng.planpro.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
