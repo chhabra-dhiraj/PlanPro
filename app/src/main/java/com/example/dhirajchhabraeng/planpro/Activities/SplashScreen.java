84944608cac94256e74d37773dac3569a34a23c9
Daman <damanpreetsingh97@gmail.com>
2018-10-02 19:38:03 +0530

    registered SplashScreen activity in manifest and made it as the launcher activity

--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/SplashScreen.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/SplashScreen.java
@@ -1,4 +1,6 @@
 package com.example.dhirajchhabraeng.planpro.Activities;
 
-public class SplashScreen {
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
 }
