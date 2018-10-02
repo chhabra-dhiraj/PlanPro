f0fed3a73739831e16dcb4d4fb1aa2804fecbaf1
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-10-03 00:53:39 +0530

    commit after removing unnecessary import statements

--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/WelcomeActivity.java
@@ -3,8 +3,6 @@ package com.example.dhirajchhabraeng.planpro.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.dhirajchhabraeng.planpro.R;
-
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
