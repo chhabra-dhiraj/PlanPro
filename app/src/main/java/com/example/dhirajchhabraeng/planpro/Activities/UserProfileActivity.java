2cfec7f99b44118fe0c3613a09582aee509ecf54
Daman <damanpreetsingh97@gmail.com>
2018-10-07 23:55:38 +0530

    added a button in User profile Activity to remove user account

--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/UserProfileActivity.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/UserProfileActivity.java
@@ -1,15 +1,58 @@
 package com.example.dhirajchhabraeng.planpro.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dhirajchhabraeng.planpro.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    Button btnDeleteAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_user_profile);
+        btnDeleteAccount = findViewById(R.id.btn_user_profile_activity_delete_account);
+        btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // Get auth credentials from the user for re-authentication. The example below shows
+                // email and password credentials but there are multiple possible providers,
+                // such as GoogleAuthProvider or FacebookAuthProvider.
+                AuthCredential credential = EmailAuthProvider
+                        .getCredential("user@example.com", "password1234");
+
+                // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                user.delete()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d("TAG", "User account deleted.");
+                                                }
+                                            }
+                                        });
+
+                            }
+                        });
+            }
+        });
     }
 }
