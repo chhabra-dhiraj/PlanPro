42fe545a500ef2a268b1e105de735f16936f6491
Daman <damanpreetsingh97@gmail.com>
2018-10-31 17:44:48 +0530

    made incomplete changes

--- app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/UserProfileActivity.java
+++ app/src/main/java/com/example/dhirajchhabraeng/planpro/Activities/UserProfileActivity.java
@@ -23,22 +23,40 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     Button btnDeleteAccount;
     EditText etEmail, etMobileNum, etFirstName, etMiddleName, etLastName, etCity;
+    CircleImageView civProfilePicture;
+    FirebaseUser currUser;
+    DatabaseReference currUserReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        currUser = FirebaseAuth.getInstance().getCurrentUser();
+        currUserReference = FirebaseDatabase.getInstance()
+                .getReference().child("Users").child(currUser.getUid());
+
         btnDeleteAccount = findViewById(R.id.btn_user_profile_activity_delete_account);
+        civProfilePicture = findViewById(R.id.civ_upa_profile_picture);
+
+        if(currUser.getPhotoUrl()!=null){
+            Picasso.get().load(currUser.getPhotoUrl()).into(civProfilePicture);
+        }else{
+            Picasso.get().load(R.drawable.ic_person_white_150dp).into(civProfilePicture);
+        }
 
         btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
 
                 // Get auth credentials from the user for re-authentication. The example below shows
                 // email and password credentials but there are multiple possible providers,
@@ -46,8 +64,8 @@ public class UserProfileActivity extends AppCompatActivity {
 
                 String password = null;
                 AuthCredential credential = EmailAuthProvider
-                        .getCredential(user.getEmail(), password);
-                reAuthenticateAndDelete(user, credential);
+                        .getCredential(currUser.getEmail(), password);
+                reAuthenticateAndDelete(currUser, credential);
 
 //                user.getIdToken(false).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
 //                    @Override
@@ -65,21 +83,19 @@ public class UserProfileActivity extends AppCompatActivity {
         });
     }
 
-    private void reAuthenticateAndDelete(final FirebaseUser user, AuthCredential credential){
-        user.reauthenticate(credential)
+    private void reAuthenticateAndDelete(final FirebaseUser currUser, AuthCredential credential){
+        currUser.reauthenticate(credential)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Log.e("TAG", "onComplete: authentication complete");
-                            user.delete()
+                            currUser.delete()
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
                                             if (task.isSuccessful()) {
                                                 Log.e("TAG", "User account deleted.");
-                                                DatabaseReference currUserReference = FirebaseDatabase.getInstance()
-                                                        .getReference().child("Users").child(user.getUid());
                                                 currUserReference.removeValue(new DatabaseReference.CompletionListener() {
                                                     @Override
                                                     public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
